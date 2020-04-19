eac1ac3cf3f32a5583644c6c73a69a71bc7a6c42
ranok92 <abhisek.konar@mail.mcgill.ca>
2020-04-19 16:58:03 -0400

    Updated the file to create pytorch compatible dataset for both skipgram and cbow.

--- word2vec/w2v_dataloader.py
+++ word2vec/w2v_dataloader.py
@@ -3,7 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 import json
 import numpy as np
 import utils as ut
-import pdb
+
 
 class CBOW_dataset():
     """CBOW dataset """
@@ -11,14 +11,14 @@ class CBOW_dataset():
     def __init__(self, cbow_json_path):
         """
         Args:
-            cbow_json : The CBOW JSON dataset path
+            cbow_json_path : The CBOW JSON dataset path
         """
         self.vocab_word_to_idx = {}
         self.vocab_set = set()
         with open(cbow_json_path) as f:
             self.json_data = json.load(f)
             for ith_words in self.json_data:
-                output, inputs = ith_words[-1], ith_words[0:-1]
+                output, inputs = ith_words[-1], ith_words[:-1]
                 self.vocab_set.add(output)
                 self.vocab_set.update(inputs)
         self.vocab_set = list(self.vocab_set)
@@ -31,12 +31,45 @@ class CBOW_dataset():
     def __getitem__(self, idx):
         words = self.json_data[idx]
         output, inputs = words[-1], words[:-1]
-        #output_ohv = ut.get_one_hot_vec(output, self.vocab_word_to_idx)
+        # output_ohv = ut.get_one_hot_vec(output, self.vocab_word_to_idx)
         output_class = self.vocab_word_to_idx[output]
         inputs_ohv = sum([ut.get_one_hot_vec(
             input, self.vocab_word_to_idx) for input in inputs])
         return inputs_ohv, output_class
 
 
+class SkipGramDataset(Dataset):
+    """Skip Gram Dataset"""
+
+    def __init__(self, skipgram_json_path):
+        """
+        Args:
+            skipgram_json_path : The SkupGram JSON dataset path
+        """
+        self.vocab_word_to_idx = {}
+        self.vocab_set = set()
+        with open(skipgram_json_path) as f:
+            self.json_data = json.load(f)
+            for ith_words in self.json_data:
+                input, output = ith_words[0], ith_words[1]
+                self.vocab_set.add(input)
+                self.vocab_set.add(output)
+        self.vocab_set = list(self.vocab_set)
+        for idx, word in enumerate(self.vocab_set):
+            self.vocab_word_to_idx[word] = idx
+
+    def __len__(self):
+        return len(self.json_data)
+
+    def __getitem__(self, idx):
+        words = self.json_data[idx]
+        input, output = words[0], words[1]
+        input_ohv = ut.get_one_hot_vec(input, self.vocab_word_to_idx)
+        # output_ohv = ut.get_one_hot_vec(output, self.vocab_word_to_idx)
+        output_class = self.vocab_word_to_idx[output]
+        return input_ohv, output_class
+
+
 if __name__ == '__main__':
     cbow_dataset = CBOW_dataset('cbow_style_training_dataset.json')
+    print(cbow_dataset[1])
